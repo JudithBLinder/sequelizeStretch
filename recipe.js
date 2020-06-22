a5124e418d62f174f06b7bf0679bd83ec75ff237
JudithBLinder <judithrooz@gmail.com>
2020-06-22 18:56:07 -0400

    Done

--- recipe.js
+++ recipe.js
@@ -1,7 +1,27 @@
 const db = require('./dbConfig');
 const Sequelize = require('sequelize');
+const { STRING, INTEGER, BOOLEAN, ENUM } = Sequelize;
 
 //YOUR CODE GOES HERE
 
+const Recipe = db.define('recipe', {
+  name: {
+    type: STRING,
+    defaultValue: 'cereal',
+    notEmpty: true,
+  },
+  cookTime: {
+    type: INTEGER,
+    min: 1,
+    max: 60,
+  },
+  vegan: {
+    type: BOOLEAN,
+  },
+  foodGroup: {
+    type: ENUM,
+    values: ['vegetable', 'dairy', 'grain', 'fruit'],
+  },
+});
 //--------------------
 module.exports = Recipe;
