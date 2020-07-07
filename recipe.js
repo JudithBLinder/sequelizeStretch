959d889bca67795565821a333344fa7c45247fc1
JudithBLinder <judithrooz@gmail.com>
2020-07-07 17:19:41 -0400

    seq_stretch_2 solved

--- recipe.js
+++ recipe.js
@@ -8,19 +8,22 @@ const Recipe = db.define('recipe', {
   name: {
     type: STRING,
     defaultValue: 'cereal',
-    notEmpty: true,
+    validate: {
+      notEmpty: true,
+    },
   },
   cookTime: {
     type: INTEGER,
-    min: 1,
-    max: 60,
+    validate: {
+      min: 1,
+      max: 60,
+    },
   },
   vegan: {
     type: BOOLEAN,
   },
   foodGroup: {
-    type: ENUM,
-    values: ['vegetable', 'dairy', 'grain', 'fruit'],
+    type: ENUM(['vegetable', 'meat', 'dairy', 'grain', 'fruit']),
   },
 });
 //--------------------
